1e502d8df5d9a9dbc52e5edde79bc028dd258882
Caleb <caleb@whataloadofrubbish.com>
2015-01-13 13:41:02 +0000

    updated test so that time is reported, runs over a larger range again

--- src/ex13/ex13.java
+++ src/ex13/ex13.java
@@ -3,16 +3,13 @@ public class ex13{
 	int[] testArray;
 	public static void main(String[] args) {
 		ex13 exercise = new ex13();
-		for (int i = 500; i < 501; i *= 2){
+		for (int i = 500; i < 30000; i += 5000){
 			exercise.test(i);
 		}
 	}
 	
 
 	private void sortIt(int[] testArray) {
-		//System.out.print(testArray.length + " ints, unsorted list: ");
-		//ListUtilities linkedList = ListUtilities.arrayToLinkedList(testArray);
-		//linkedList.listObjectsFromHead();
 		System.out.print("bubble sorted list:");
 		ListUtilities bubblesort = ListUtilities.bubbleSort(ListUtilities.arrayToLinkedList(testArray));
 		System.out.println(testArray.length + " items sorted in " + bubblesort.getLastExecTime() + "ms.");
@@ -24,7 +21,7 @@ public class ex13{
 		
 		System.out.print("quick sorted list:");
 		ListUtilities quicksort = ListUtilities.quickSort(ListUtilities.arrayToLinkedList(testArray));
-		//System.out.println(testArray.length + " items sorted in " + quicksort.getLastExecTime() + "ms.");
+		System.out.println(testArray.length + " items sorted in " + quicksort.getLastExecTime() + "ms.");
 	}
 	
 	private void test(int length){
